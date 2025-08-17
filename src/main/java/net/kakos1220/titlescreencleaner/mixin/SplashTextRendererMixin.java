d4c8b6d11c574d93a729ad60e5f935db7d2b2930
KAkos1220 <KAkosyyy@gmail.com>
2025-08-17 02:38:26 +0200

    Initial commit

---
+++ src/main/java/net/kakos1220/titlescreencleaner/mixin/SplashTextRendererMixin.java
@@ -0,0 +1,19 @@
+package net.kakos1220.titlescreencleaner.mixin;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.SplashTextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(SplashTextRenderer.class)
+public class SplashTextRendererMixin {
+    /**
+     * @author KAkos1220
+     * @reason Removes SplashText rendering entirely
+     */
+    @Overwrite
+    public void render(DrawContext context, int screenWidth, TextRenderer textRenderer, float alpha) {
+
+    }
+}
---
+++ src/main/java/net/kakos1220/titlescreencleaner/mixin/TitleScreenMixin.java
@@ -0,0 +1,61 @@
+package net.kakos1220.titlescreencleaner.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.font.TextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Environment(EnvType.CLIENT)
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+    @Redirect(
+            method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/TitleScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;",
+                    ordinal = 0
+            )
+    )
+    private Element removeLanguageButton(TitleScreen instance, Element element) {
+        return element;
+    }
+
+    @Redirect(
+            method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/TitleScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;",
+                    ordinal = 3
+            )
+    )
+    private Element removeAccessibilityButton(TitleScreen instance, Element element) {
+        return element;
+    }
+
+    @Redirect(
+            method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/TitleScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;",
+                    ordinal = 4
+            )
+    )
+    private Element removeCopyrightText(TitleScreen instance, Element element) {
+        return element;
+    }
+
+    @Redirect(
+            method = "render(Lnet/minecraft/client/gui/DrawContext;IIF)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"
+            )
+    )
+    private void removeVersionText(DrawContext context, TextRenderer textRenderer, String text, int x, int y, int color) {
+    }
+}
