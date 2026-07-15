6d8265aac43b05fafa65447567c77e0a927623e5
KAkos1220 <KAkosyyy@gmail.com>
2026-07-15 17:59:36 +0200

    Updated 26.2

--- src/main/java/net/kakos1220/titlescreencleaner/mixin/TitleScreenMixin.java
+++ src/main/java/net/kakos1220/titlescreencleaner/mixin/TitleScreenMixin.java
@@ -2,51 +2,71 @@ package net.kakos1220.titlescreencleaner.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.components.*;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.gui.screens.friends.FriendsOverlayScreen;
+import net.minecraft.client.gui.screens.options.OnlineOptionsScreen;
+import net.minecraft.client.gui.screens.options.OptionsScreen;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
-    @Redirect(
-            method = "init",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;",
-                    ordinal = 0
-            )
-    )
-    private GuiEventListener removeLanguageButton(TitleScreen instance, GuiEventListener element) {
-        return element;
+public class TitleScreenMixin extends Screen {
+
+    protected TitleScreenMixin(Component title) {
+        super(title);
     }
 
-    @Redirect(
-            method = "init",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;",
-                    ordinal = 3
-            )
-    )
-    private GuiEventListener removeAccessibilityButton(TitleScreen instance, GuiEventListener element) {
-        return element;
-    }
+    @Shadow @Nullable private SplashRenderer splash;
+    @Shadow @Nullable private FriendsButton friends;
 
-    @Redirect(
-            method = "init",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;",
-                    ordinal = 4
-            )
-    )
-    private GuiEventListener removeCopyrightText(TitleScreen instance, GuiEventListener element) {
-        return element;
+    @Shadow private int createNormalMenuOptions(int topPos, int spacing) { throw new AssertionError(); }
+    @Shadow private int createDemoMenuOptions(int topPos, int spacing) { throw new AssertionError(); }
+
+    @Overwrite
+    protected void init() {
+        if (this.splash == null) {
+            this.splash = this.minecraft.gui.splashManager().getSplash();
+        }
+
+        int topPos = this.height / 4 + 48;
+        if (this.minecraft.isDemo()) {
+            topPos = this.createDemoMenuOptions(topPos, 24);
+        } else {
+            topPos = this.createNormalMenuOptions(topPos, 24);
+        }
+        topPos += 36;
+
+        this.addRenderableWidget(
+                Button.builder(Component.translatable("menu.options"),
+                                button -> this.minecraft.gui.setScreen(new OptionsScreen(this, this.minecraft.options, false)))
+                        .bounds(this.width / 2 - 100, topPos, 86, 20)
+                        .build()
+        );
+
+        this.friends = this.addRenderableWidget(
+                CommonButtons.friends(
+                        20,
+                        button -> OnlineOptionsScreen.confirmFriendsListEnabled(this.minecraft, () -> this.minecraft.gui.setScreen(new FriendsOverlayScreen(this)), this),
+                        !this.minecraft.isDemo()
+                )
+        );
+        this.friends.setPosition(this.width / 2 - 10, topPos);
+
+        this.addRenderableWidget(
+                Button.builder(Component.translatable("menu.quit"),
+                                button -> this.minecraft.stop())
+                        .bounds(this.width / 2 + 14, topPos, 86, 20)
+                        .build()
+        );
     }
 
     @Inject(
