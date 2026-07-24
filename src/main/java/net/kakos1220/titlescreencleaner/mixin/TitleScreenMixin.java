9fee60567edb290dedb7471fff0e33a75885c5c9
KAkos1220 <KAkosyyy@gmail.com>
2026-07-24 03:01:07 +0200

    Fixed crash when loaded with ModMenu

--- src/main/java/net/kakos1220/titlescreencleaner/mixin/TitleScreenMixin.java
+++ src/main/java/net/kakos1220/titlescreencleaner/mixin/TitleScreenMixin.java
@@ -1,5 +1,6 @@
 package net.kakos1220.titlescreencleaner.mixin;
 
+import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.components.*;
@@ -16,9 +17,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
-@Mixin(TitleScreen.class)
+@Mixin(value = TitleScreen.class, priority = 900)
 public class TitleScreenMixin extends Screen {
 
     protected TitleScreenMixin(Component title) {
@@ -27,15 +29,19 @@ public class TitleScreenMixin extends Screen {
 
     @Shadow @Nullable private SplashRenderer splash;
     @Shadow @Nullable private FriendsButton friends;
+    @Shadow @Nullable private RealmsNotificationsScreen realmsNotificationsScreen;
 
     @Shadow private int createNormalMenuOptions(int topPos, int spacing) { throw new AssertionError(); }
     @Shadow private int createDemoMenuOptions(int topPos, int spacing) { throw new AssertionError(); }
+    @Shadow private boolean realmsNotificationsEnabled() { throw new AssertionError(); }
+    @Shadow private int getHorizontalPosition(int currentButton, int numberOfButtons, int buttonWidth) { throw new AssertionError(); }
 
     @Overwrite
     protected void init() {
         if (this.splash == null) {
             this.splash = this.minecraft.gui.splashManager().getSplash();
         }
+
 
         int topPos = this.height / 4 + 48;
         if (this.minecraft.isDemo()) {
@@ -45,28 +51,46 @@ public class TitleScreenMixin extends Screen {
         }
         topPos += 36;
 
-        this.addRenderableWidget(
-                Button.builder(Component.translatable("menu.options"),
-                                button -> this.minecraft.gui.setScreen(new OptionsScreen(this, this.minecraft.options, false)))
-                        .bounds(this.width / 2 - 100, topPos, 86, 20)
-                        .build()
-        );
+
+        int buttonWidth = 20;
+        int numberOfButtons = 1;
+        int currentButton = 0;
+
 
         this.friends = this.addRenderableWidget(
                 CommonButtons.friends(
-                        20,
+                        buttonWidth,
                         button -> OnlineOptionsScreen.confirmFriendsListEnabled(this.minecraft, () -> this.minecraft.gui.setScreen(new FriendsOverlayScreen(this)), this),
                         !this.minecraft.isDemo()
                 )
         );
-        this.friends.setPosition(this.width / 2 - 10, topPos);
+        this.friends.setPosition(this.getHorizontalPosition(++currentButton, numberOfButtons, buttonWidth), topPos);
+
+
+        int totalWidth = numberOfButtons * buttonWidth + (numberOfButtons - 1) * 4;
+
+        this.addRenderableWidget(
+                Button.builder(Component.translatable("menu.options"),
+                                button -> this.minecraft.gui.setScreen(new OptionsScreen(this, this.minecraft.options, false)))
+                        .bounds(this.width / 2 - 100, topPos, 100 - (totalWidth / 2 + 4), 20)
+                        .build()
+        );
 
         this.addRenderableWidget(
                 Button.builder(Component.translatable("menu.quit"),
                                 button -> this.minecraft.stop())
-                        .bounds(this.width / 2 + 14, topPos, 86, 20)
+                        .bounds(this.width / 2 + totalWidth / 2 + 4, topPos, 100 - (totalWidth / 2 + 4), 20)
                         .build()
         );
+
+
+        if (this.realmsNotificationsScreen == null) {
+            this.realmsNotificationsScreen = new RealmsNotificationsScreen();
+        }
+
+        if (this.realmsNotificationsEnabled()) {
+            this.realmsNotificationsScreen.init(this.width, this.height);
+        }
     }
 
     @Inject(
@@ -79,5 +103,14 @@ public class TitleScreenMixin extends Screen {
     )
     private void removeVersionText(CallbackInfo ci) {
         ci.cancel();
+    }
+
+    @Inject(
+            method = "realmsNotificationsEnabled",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void removeRealmsNotifications(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
     }
 }
